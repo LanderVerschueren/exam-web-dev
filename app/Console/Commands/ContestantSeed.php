f62e2580ed4d8d930f59a4f466630389ed7dc42e
Lander Verschueren <verschueren@live.nl>
2016-11-02 10:14:39 +0000

    db insert via cronjob -> factory

--- app/Console/Commands/ContestantSeed.php
+++ app/Console/Commands/ContestantSeed.php
@@ -40,26 +40,6 @@ class ContestantSeed extends Command
      */
     public function handle(\Faker\Generator $faker)
     {
-        /*$contestant = factory(Contestant::class)->create();
-        echo $contestant;*/
-        $contests = Contest::all();
-        $current_date = \Carbon\Carbon::now()->toDateTimeString();
-
-        foreach( $contests as $contest ) {
-            if( $contest->start_date < $current_date && $contest->end_date >= $current_date ) {
-                $contest_id = $contest->id;
-            }
-        }
-        //$contestant = factory(Contestant::class)->create();
-        DB::table('contestants')->insert([
-            'code' => $faker->unique()->randomNumber,
-            'name' => $faker->name,
-            'adres' => $faker->streetName,
-            'woonplaats' => $faker->city,
-            'ip' => $faker->ipv4,
-            'contest_id' => $contest_id,
-            'created_at' => $current_date,
-            'updated_at' => $current_date
-        ]);
+        factory(Contestant::class)->create();
     }
 }
